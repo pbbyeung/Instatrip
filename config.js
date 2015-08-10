5e1d0c78eacf26d935d65724e1cbcb9ad6e71757
pbbyeung <pbbyeung@gmail.com>
2015-08-10 12:34:06 -0700

    [fix] add env file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  InstaClientID: process.env.InstaClientID,
+  InstaClientSecret: process.env.InstaClientSecret,
+  use_database: 'false',
+  callback_url: process.env.callback_url,
+  access_token: null,
+  EchoApiKey: process.env.EchoApiKey,
+  EchoSecret: process.env.EchoSecret,
+};
